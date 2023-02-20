425a634c3cb78f8d7fb9b639ddfbecf206015f60
GoodKevin <39488346+goodkevin0721@users.noreply.github.com>
2023-02-20 18:36:29 +0800

    添加部分逻辑

---
+++ Assets/Deer/Scripts/Hotfix/HotfixFramework/Runtime/Component/CarMove/CarMoveComponent.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using cfg.Deer;
+using cfg.Deer.Enum;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+public class CarMoveComponent : GameFrameworkComponent
+{
+    // private CarMoveLogic m_CarMoveData;
+    // public CarMoveLogic CarMoveData { get { return m_CarMoveData; }}
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Camera = Camera.main;
+        //m_DefaultPos = m_CarMoveData.;
+    }
+
+    // Update is called once per frame
+    private Vector3 mMouseLastPos = Vector3.zero;
+    private bool mMouseDown = false;
+    private Camera m_Camera;
+    private Vector3 m_DefaultPos = Vector3.zero;
+    private CarMoveType m_MoveType = CarMoveType.HORIZONTAL;
+    private Transform m_SelectTransform;
+
+    private void Update()
+    {
+        if (m_SelectTransform == null)
+        {
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            mMouseDown = true;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            mMouseDown = false;
+            mMouseLastPos = Vector3.zero;
+        }
+
+        if (mMouseDown)
+        {
+            Vector3 tempPos = m_Camera.ScreenToWorldPoint(Input.mousePosition);
+
+            if (mMouseLastPos != Vector3.zero)
+            {
+                Vector3 tempMoveOffset = tempPos - mMouseLastPos;
+                float tempLimitOffset = 0;
+                if (m_MoveType == CarMoveType.HORIZONTAL)
+                {
+                    RaycastHit2D tempRightRay = SendRay(tempMoveOffset, Vector2.right, 0.2f);
+                    RaycastHit2D tempLeftRay = SendRay(tempMoveOffset, Vector2.left, 0.2f);
+                    if (tempRightRay)
+                    {
+                        if (tempMoveOffset.x < 0)
+                        {
+                            tempLimitOffset = tempMoveOffset.x;
+                        }
+                        else
+                        {
+                            tempLimitOffset = 0;
+                        }
+                    }
+                    else if (tempLeftRay)
+                    {
+                        if (tempMoveOffset.x > 0)
+                        {
+                            tempLimitOffset = tempMoveOffset.x;
+                        }
+                        else
+                        {
+                            tempLimitOffset = 0;
+                        }
+                    }
+
+                    m_SelectTransform.position =
+                        new Vector3(m_SelectTransform.position.x + tempLimitOffset, m_DefaultPos.y, 0);
+                }
+                else
+                {
+                    RaycastHit2D tempUpRay = SendRay(tempMoveOffset, Vector2.up, 0.2f);
+                    RaycastHit2D tempDownRay = SendRay(tempMoveOffset, Vector2.down, 0.2f);
+                    if (tempUpRay)
+                    {
+                        if (tempMoveOffset.y < 0)
+                        {
+                            tempLimitOffset = tempMoveOffset.y;
+                        }
+                        else
+                        {
+                            tempLimitOffset = 0;
+                        }
+                    }
+                    else if (tempDownRay)
+                    {
+                        if (tempMoveOffset.y > 0)
+                        {
+                            tempLimitOffset = tempMoveOffset.y;
+                        }
+                        else
+                        {
+                            tempLimitOffset = 0;
+                        }
+                    }
+
+                    m_SelectTransform.position =
+                        new Vector3(m_DefaultPos.x, m_SelectTransform.position.y + tempLimitOffset, 0);
+                }
+            }
+
+            mMouseLastPos = tempPos;
+        }
+    }
+
+    private RaycastHit2D SendRay(Vector3 tempOffset, Vector2 tempDirection, float distance)
+    {
+        Vector3 tempPos = m_SelectTransform.position + tempOffset;
+        RaycastHit2D tempRayHit =
+            Physics2D.Raycast(tempPos, tempDirection, distance, 1 << LayerMask.NameToLayer("Move"));
+        Debug.DrawRay(tempPos, tempDirection, tempRayHit ? Color.red : Color.green);
+        if (tempDirection == Vector2.left || tempDirection == Vector2.right)
+        {
+            Debug.DrawLine(new Vector3(tempPos.x, tempPos.y + 0.2f, 0),
+                new Vector3(tempPos.x, tempPos.y + 0.2f, 0));
+        }
+        else
+        {
+            Debug.DrawLine(new Vector3(tempPos.x + 0.2f, tempPos.y, 0),
+                new Vector3(tempPos.x + 0.2f, tempPos.y, 0));
+        }
+
+        return tempRayHit;
+    }
+}
