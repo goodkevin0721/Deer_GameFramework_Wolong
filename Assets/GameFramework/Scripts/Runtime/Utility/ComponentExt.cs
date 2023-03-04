4935143339aeec871b02abccbb9ce1780c32b3fb
GoodKevin <39488346+goodkevin0721@users.noreply.github.com>
2023-03-05 01:01:03 +0800

    完善移动逻辑

---
+++ Assets/GameFramework/Scripts/Runtime/Utility/ComponentExt.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityGameFramework.Runtime
+{
+    public static class ComponentExt
+    {
+        public static T AddComponent<T>(this Component component) where T : Component =>
+            component.gameObject.AddComponent<T>();
+
+        public static T GetComponent<T>(this Component component, string name) where T : Component
+        {
+            if (string.IsNullOrEmpty(name))
+                return component.GetComponent<T>();
+            Transform transform = component.transform.Find(name);
+            return (UnityEngine.Object)transform == (UnityEngine.Object)null ? default(T) : transform.GetComponent<T>();
+        }
+
+        public static T GetComponent<T>(this Component component, int index) where T : Component
+        {
+            if (index < 0 || index >= component.transform.childCount)
+                return component.GetComponent<T>();
+            Transform child = component.transform.GetChild(index);
+            return (UnityEngine.Object)child == (UnityEngine.Object)null ? default(T) : child.GetComponent<T>();
+        }
+
+        public static T GetComponent2<T>(this Component component) where T : Component
+        {
+            T component2 = component.GetComponent<T>();
+            if ((UnityEngine.Object)component2 == (UnityEngine.Object)null)
+                component2 = component.GetComponentInChildren<T>(true);
+            return component2;
+        }
+
+        public static T GetComponent2<T>(this Component component, string name) where T : Component
+        {
+            if (string.IsNullOrEmpty(name))
+                return component.GetComponent2<T>();
+            Transform transform = component.transform.Find(name);
+            if ((UnityEngine.Object)transform == (UnityEngine.Object)null)
+            {
+                transform = component.transform.Find2(name);
+                if ((UnityEngine.Object)transform == (UnityEngine.Object)null)
+                    return default(T);
+            }
+
+            return transform.GetComponent<T>();
+        }
+
+        public static T GetComponentOrAdd<T>(this Component component) where T : Component
+        {
+            T componentOrAdd = component.GetComponent<T>();
+            if ((UnityEngine.Object)componentOrAdd == (UnityEngine.Object)null)
+                componentOrAdd = component.gameObject.AddComponent<T>();
+            return componentOrAdd;
+        }
+
+        public static T GetComponentOrAdd<T>(this Component component, string name) where T : Component
+        {
+            if (string.IsNullOrEmpty(name))
+                return component.GetComponentOrAdd<T>();
+            Transform component1 = component.transform.Find(name);
+            if ((UnityEngine.Object)component1 == (UnityEngine.Object)null)
+            {
+                component1 = component.transform.Find2(name);
+                if ((UnityEngine.Object)component1 == (UnityEngine.Object)null)
+                    return default(T);
+            }
+
+            return component1.GetComponentOrAdd<T>();
+        }
+
+        public static T GetComponentOrAdd<T>(this Component component, int index) where T : Component
+        {
+            if (index < 0 || index >= component.transform.childCount)
+                return component.GetComponentOrAdd<T>();
+            Transform child = component.transform.GetChild(index);
+            return (UnityEngine.Object)child == (UnityEngine.Object)null ? default(T) : child.GetComponentOrAdd<T>();
+        }
+
+        public static void SetFather(this Component component, Transform parent) => component.SetFather(parent, true);
+
+        public static void SetFather(this Component component, Transform parent, bool reset)
+        {
+            if ((UnityEngine.Object)component.transform.parent != (UnityEngine.Object)parent)
+                component.transform.SetParent(parent);
+            if (!reset)
+                return;
+            RectTransform transform = component.transform as RectTransform;
+            if ((UnityEngine.Object)transform != (UnityEngine.Object)null)
+            {
+                transform.anchoredPosition = Vector2.zero;
+                transform.sizeDelta = Vector2.zero;
+            }
+            else
+            {
+                component.transform.localPosition = Vector3.zero;
+                component.transform.localRotation = Quaternion.identity;
+                component.transform.localScale = Vector3.one;
+            }
+        }
+
+        public static void SetFather(this Component component, Transform parent, string name)
+        {
+            if ((UnityEngine.Object)parent == (UnityEngine.Object)null || string.IsNullOrEmpty(name))
+            {
+                component.SetFather(parent, true);
+            }
+            else
+            {
+                Transform parent1 = parent.Find2(name);
+                component.SetFather(parent1, true);
+            }
+        }
+
+        public static void SetLayerBy(this Component component, int index)
+        {
+            int num = 1 << index;
+            if (component.gameObject.layer == num)
+                return;
+            component.gameObject.layer = num;
+        }
+
+        public static void SetLayer(this Component component, int layer)
+        {
+            if (component.gameObject.layer == layer)
+                return;
+            component.gameObject.layer = layer;
+        }
+
+        public static void SetLayer(this Component component, string name)
+        {
+            int layer = LayerMask.NameToLayer(name);
+            if (component.gameObject.layer == layer)
+                return;
+            component.gameObject.layer = layer;
+        }
+
+        public static void SetActive(this Component component, bool active)
+        {
+            if (component.gameObject.activeSelf == active)
+                return;
+            component.gameObject.SetActive(active);
+        }
+
+        public static void SafeDestroy(this Component component) => ComponentExt.SafeDestroy(component, true);
+
+        public static void SafeDestroy(this Component component, bool includeAsset)
+        {
+            if ((UnityEngine.Object)component == (UnityEngine.Object)null)
+                return;
+            try
+            {
+                UnityEngine.Object.Destroy((UnityEngine.Object)component);
+                if (!includeAsset)
+                    return;
+                UnityEngine.Object.DestroyImmediate((UnityEngine.Object)component.gameObject, true);
+            }
+            catch (MissingComponentException ex)
+            {
+                Log.Fatal((Exception)ex);
+            }
+            catch (MissingReferenceException ex)
+            {
+                Log.Fatal((Exception)ex);
+            }
+            catch (NullReferenceException ex)
+            {
+                Log.Fatal((Exception)ex);
+            }
+            catch (UnassignedReferenceException ex)
+            {
+                Log.Fatal((Exception)ex);
+            }
+        }
+
+        public static Component AddComponent(this Component component, System.Type type) =>
+            component.gameObject.AddComponent(type);
+
+        public static Component GetComponent(this Component component, System.Type type, string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                return component.GetComponent(type);
+            Transform transform = component.transform.Find(name);
+            return (UnityEngine.Object)transform == (UnityEngine.Object)null
+                ? (Component)null
+                : transform.GetComponent(type);
+        }
+
+        public static Component GetComponent(this Component component, System.Type type, int index)
+        {
+            if (index < 0 || index >= component.transform.childCount)
+                return component.GetComponent(type);
+            Transform child = component.transform.GetChild(index);
+            return (UnityEngine.Object)child == (UnityEngine.Object)null ? (Component)null : child.GetComponent(type);
+        }
+
+        public static Component GetComponent2(this Component component, System.Type type)
+        {
+            Component component2 = component.GetComponent(type);
+            if ((UnityEngine.Object)component2 == (UnityEngine.Object)null)
+                component2 = component.GetComponentInChildren(type, true);
+            return component2;
+        }
+
+        public static Component GetComponent2(
+            this Component component,
+            System.Type type,
+            string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                return component.GetComponent2(type);
+            Transform transform = component.transform.Find(name);
+            if ((UnityEngine.Object)transform == (UnityEngine.Object)null)
+            {
+                transform = component.transform.Find2(name);
+                if ((UnityEngine.Object)transform == (UnityEngine.Object)null)
+                    return (Component)null;
+            }
+
+            return transform.GetComponent(type);
+        }
+
+        public static Component GetComponentOrAdd(this Component component, System.Type type)
+        {
+            Component componentOrAdd = component.GetComponent(type);
+            if ((UnityEngine.Object)componentOrAdd == (UnityEngine.Object)null)
+                componentOrAdd = component.gameObject.AddComponent(type);
+            return componentOrAdd;
+        }
+
+        public static Component GetComponentOrAdd(
+            this Component component,
+            System.Type type,
+            string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                return component.GetComponentOrAdd(type);
+            Transform component1 = component.transform.Find(name);
+            if ((UnityEngine.Object)component1 == (UnityEngine.Object)null)
+            {
+                component1 = component.transform.Find2(name);
+                if ((UnityEngine.Object)component1 == (UnityEngine.Object)null)
+                    return (Component)null;
+            }
+
+            return component1.GetComponentOrAdd(type);
+        }
+
+        public static Component GetComponentOrAdd(
+            this Component component,
+            System.Type type,
+            int index)
+        {
+            if (index < 0 || index >= component.transform.childCount)
+                return component.GetComponentOrAdd(type);
+            Transform child = component.transform.GetChild(index);
+            return (UnityEngine.Object)child == (UnityEngine.Object)null
+                ? (Component)null
+                : child.GetComponentOrAdd(type);
+        }
+    }
+}
---
+++ Assets/GameFramework/Scripts/Runtime/Utility/TransformExt.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+
+namespace UnityGameFramework.Runtime
+{
+    public static class TransformExt
+    {
+        public static Transform Find2(this Transform transform, string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                return transform;
+            for (int index = 0; index < transform.childCount; ++index)
+            {
+                Transform child = transform.GetChild(index);
+                if (child.name == name)
+                    return child;
+                if (child.childCount > 0)
+                {
+                    Transform transform1 = child.Find2(name);
+                    if ((Object)transform1 != (Object)null)
+                        return transform1;
+                }
+            }
+
+            return (Transform)null;
+        }
+
+        public static void LookAt2(this Transform transform, Transform target, Vector3 worldUp)
+        {
+            if (!((Object)target != (Object)null) || !((Object)target != (Object)transform))
+                return;
+            transform.LookAt2(target.position, worldUp);
+        }
+
+        public static void LookAt2(this Transform transform, Transform target)
+        {
+            if (!((Object)target != (Object)null) || !((Object)target != (Object)transform))
+                return;
+            transform.LookAt2(target.position, Vector3.up);
+        }
+
+        public static void LookAt2(this Transform transform, Vector3 position, Vector3 worldUp)
+        {
+            if ((Object)transform == (Object)null || transform.position == position)
+                return;
+            Vector3 toDirection = position - transform.position;
+            if (toDirection.normalized == Vector3.back)
+                transform.rotation = new Quaternion(0.0f, 1f, 0.0f, 0.0f);
+            else
+                transform.rotation = Quaternion.FromToRotation(Vector3.forward, toDirection);
+        }
+
+        public static void LookAt2(this Transform transform, Vector3 position) =>
+            transform.LookAt2(position, Vector3.up);
+
+        public static void LookAtXZ(this Transform transform, Transform target)
+        {
+            if (!((Object)target != (Object)null) || !((Object)target != (Object)transform))
+                return;
+            transform.LookAtXZ(target.position);
+        }
+
+        public static void LookAtXZ(this Transform transform, Vector3 position)
+        {
+            position.y = transform.position.y;
+            transform.LookAt2(position);
+        }
+
+      
+        public static Vector3 TransformPoint2(this Transform transform, Vector3 position)
+        {
+            for (; (Object)transform != (Object)null; transform = transform.parent)
+            {
+                position.Scale(transform.localScale);
+                position = transform.localRotation * position;
+                position += transform.localPosition;
+            }
+
+            return position;
+        }
+
+        public static Vector3 TransformPoint2(this Transform transform, int x, int y, int z) =>
+            transform.TransformPoint2(new Vector3((float)x, (float)y, (float)z));
+
+        public static void Translate2(this Transform transform, Vector3 translation) =>
+            transform.position += transform.rotation * translation;
+
+        public static void Translate2(this Transform transform, Vector3 translation, Space relativeTo)
+        {
+            if (relativeTo == Space.World)
+                transform.position += translation;
+            else
+                transform.position += transform.rotation * translation;
+        }
+
+        public static void Translate2(this Transform transform, float x, float y, float z) =>
+            transform.Translate2(new Vector3(x, y, z));
+
+        public static void Translate2(
+            this Transform transform,
+            float x,
+            float y,
+            float z,
+            Space relativeTo)
+        {
+            transform.Translate2(new Vector3(x, y, z), relativeTo);
+        }
+
+        public static void Translate(this Transform transform, Vector3 translation, float distance)
+        {
+            translation *= distance;
+            transform.Translate2(translation);
+        }
+
+        public static void Translate(
+            this Transform transform,
+            Vector3 translation,
+            float distance,
+            Space relativeTo)
+        {
+            translation *= distance;
+            transform.Translate2(translation, relativeTo);
+        }
+    }
+}
